64225132a094658033dc30cb680052a515fd6153
Dheeraj-2205 <djsays124@gmail.com>
2023-08-24 00:26:57 +0530

    getproductdetails is completed

--- Backend/Controllers/getAllProduct.js
+++ Backend/Controllers/getAllProduct.js
@@ -77,3 +77,27 @@ exports.deleteProduct = async (req,res,next) =>{
         })
     }
 }
+
+exports.getProductDetails = async( req,res ) =>{
+    const {id} = req.params;
+    try {
+        const product = await Product.findById(id);
+
+        if (!product) {
+            res.status(404).json({
+                message : "Product Not Found",
+                success : false
+            })
+        } 
+        res.status(200).json({
+            success : false,
+            product
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            error
+        })
+    }
+}
--- Backend/Routes/productRoute.js
+++ Backend/Routes/productRoute.js
@@ -1,10 +1,12 @@
 const express= require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct } = require("../Controllers/getAllProduct");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../Controllers/getAllProduct");
 
 const router = express.Router();
 
+
 router.route("/products").get(getAllProducts)
 router.route("/product/create").post(createProduct)
-router.route("/product/:id").put(updateProduct)
-router.route("/product/:id").delete(deleteProduct)
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
+
+
 module.exports = router;
