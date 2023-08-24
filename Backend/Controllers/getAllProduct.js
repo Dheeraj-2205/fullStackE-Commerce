a44ff4e86dd5fefe608c1ab88bbd332124e85ade
Dheeraj-2205 <djsays124@gmail.com>
2023-08-24 17:30:01 +0530

    trying to error implement error handler

--- Backend/Controllers/getAllProduct.js
+++ Backend/Controllers/getAllProduct.js
@@ -1,103 +1,95 @@
 const Product = require("../Models/productModel.js");
-
+const ErrorHander = require("../utils/errorHandling");
 
 //Create Product => admin routes
-exports.createProduct = async(req,res,next) =>{
-    try {
-        const create = await Product.create(req.body);
-        res.status(201).json({
-            success : true,
-            message : "Product is Created Successfully ",
-            create
-        })
-    } catch (error) {
-        res.status(500).json({
-            success : false,
-            message : error.message
-        })
-    }
-}
+exports.createProduct = async (req, res, next) => {
+  try {
+    const create = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      message: "Product is Created Successfully ",
+      create,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 // createData
-exports.getAllProducts = async(req,res) =>{
-    const products = await Product.find()
-    res.status(200).json({
-        success : true,
-        products 
-    })
-}
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
 
 //updateProduct  only for admin
-exports.updateProduct = async(req,res,next)=>{
-    const {id} = req.params;
-    try {
-        const product = await Product.findById({_id : id});
-        if(!product){
-            return res.status(404).json({
-                message : "Product Is Not Found",
-                success : false
-            })
-        }
-        product = await Product.findByIdAndUpdate({_id : id}, req.body);
-        
-        res.status(200).json({
-            success : true,
-            product
-        })
-        
-    } catch (error) {
-        res.status(500).json({
-            success : false,
-            error
-        })
+exports.updateProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById({ _id: id });
+    if (!product) {
+      return res.status(404).json({
+        message: "Product Is Not Found",
+        success: false,
+      });
     }
-}
+    product = await Product.findByIdAndUpdate({ _id: id }, req.body);
 
-exports.deleteProduct = async (req,res,next) =>{
-    try {
-        const {id} = req.params;
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+};
 
-        const product = await Product.findById(id);
+exports.deleteProduct = async (req, res, next) => {
+//   try {
+    const { id } = req.params;
 
-        if(!product){
-            return res.status(404).json({
-                success : false,
-                message : "Product Not Found"
-            })
-        }
-            await Product.findByIdAndDelete(id);
-            res.status(200).json({
-                success : true,
-                message : "Delete the Product"
-            })
-        
-    } catch (error) {
-        res.status(500).json({
-            message : false,
-            error
-        })
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product Not Found",
+      });
     }
-}
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "Delete the Product",
+    });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: false,
+//       error,
+//     });
+//   }
+};
 
-exports.getProductDetails = async( req,res ) =>{
-    const {id} = req.params;
-    try {
-        const product = await Product.findById(id);
+exports.getProductDetails = async (req, res, next) => {
+  const { id } = req.params;
 
-        if (!product) {
-            return res.status(404).json({
-                message : "Product Not Found",
-                success : false
-            })
-        } 
-        res.status(200).json({
-            success : false,
-            product
-        })
+  const product = await Product.findById(id);
+  console.log(product);
 
-    } catch (error) {
-        res.status(500).json({
-            success : false,
-            message : error.message
-        })
-    }
+  if (!product) {
+    return res.status(500).json({
+        success : false,
+        message : "Product Not Found"
+    })
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
 }
--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const product = require("./Routes/productRoute.js");
 const app = express();
+// const errorHandler = require("./middleware/error.js")
 
 app.use(express.json());
 
 app.use("/mern", product);
 
+
+// middleware for error Handling
+
+// app.use(errorHandler)
 
 module.exports = app;
