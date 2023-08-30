1a4b8103004046c516cad1659cb26b0868b5986f
Dheeraj-2205 <djsays124@gmail.com>
2023-08-31 00:56:09 +0530

    adding nodemailer

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,9 +1,9 @@
 
-const asyncError = require("../middleware/asyncError.js");
 const Asynchandler = require("../middleware/asyncError.js");
 const User = require ("../Models/userModel.js");
 const ErrorHandler = require("../utils/errorHandling.js");
 const sendToken = require("../utils/jwtToken.js");
+const sendEmail = require("../utils/sendEmail.js");
 
 // register
 
@@ -32,7 +32,7 @@ exports.loginUser = Asynchandler(async(req,res,next)=>{
     const user = await User.findOne({email}).select("+password");
 
     if(!user){
-        return next(new ErrorHandler("Please Enter Email And Password", 401));
+        return next(new ErrorHandler("User does Not exists", 401));
     }
 
     const isPasswordMatch = await user.comparePassword(password);
@@ -61,10 +61,40 @@ exports.logout = Asynchandler (async(req,res,next)=>{
 
 
 exports.forgetPassword = Asynchandler(async(req,res,next)=>{
-    const user= await User.findById({email : req.body.email});
+    const user= await User.findOne({email : req.body.email});
 
     if(!user){
         return next(new ErrorHandler("User Not Found" , 404));
     }
-    const resetToken = User.resetPasswordToken()
+    
+
+    // getReset Password tOken
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({validateBeforeSave : false});
+
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/mern/password/reset/${resetToken}`
+
+    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\n If you have not requested this email
+    then please ignore it`;
+
+    try {
+        await sendEmail ({
+            email : user.email,
+            subject : `Ecommerce Password Reset`,
+            message
+        })
+        res.status(200).json({
+            success : true,
+            message : `Email sent ${user.email} successfully`
+        })
+    } catch (error) {
+        user.resetPasswordExpire = undefined;
+        user.resetPasswordToken = undefined;
+        console.log(error);
+        await uesr.save({validateBeforeSave :false})
+        return next(new ErrorHandler (error.message , 500));
+    }
+
+
 })
--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { register, loginUser, logout } = require("../Controllers/userController");
+const { register, loginUser, logout, forgetPassword } = require("../Controllers/userController");
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(loginUser);
+router.route("/forget/password").post(forgetPassword);
 router.route("/logout").get(logout);
 
 module.exports = router
