c5189c3c56c72568df8349432be594b561fb49af
Dheeraj-2205 <djsays124@gmail.com>
2023-09-10 01:58:16 +0530

    adding new component and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
-import Header from "../component/layout/Header.jsx"
+import Header from './component/layout/Header/Header';
+import { BrowserRouter ,Routes,Route } from 'react-router-dom';
+import webFont from "webfontloader";
+import { useEffect } from 'react';
+import Footer from './component/layout/Footer/Footer';
+import Home from "./component/Home/Home.jsx"
+
+
+
+
 function App() {
+  useEffect(()=>{
+    webFont.load({
+      google : {
+        families : ["Roboto","Chilanka"]
+      }
+    })
+  },[]);
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route index element = {<Home/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
+    
   );
 }
 
