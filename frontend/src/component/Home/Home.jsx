fc82ff6f884c548fd4c7e7f704ee3976e7a8db54
Dheeraj-2205 <djsays124@gmail.com>
2023-09-10 23:32:12 +0530

    Adding css

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import {CgMouse} from "react-icons/cg"
+import "./home.css"
+import Product from "./Product.jsx"
 
+const product = {
+  name : "TSHirt",
+  price : 300,
+  _id : 14,
+  images : [{url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1NMGzZlhwF5tOpnbvpAUoACBCg3QDbulPdw&usqp=CAU"}]
+}
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='banner'>
+        <p>Welcome To ECommerce</p>
+        <h1>Find Amazing Products</h1>
+
+        <a href="#container">
+          <button>Scroll <CgMouse/></button>
+        </a>
+      </div>
+      <div className="homeHeading">Feature Product</div>
+
+      <div className="container" id="container">
+        {/* redux */}
+        <Product product = {product}/> 
+      </div>
+    </>
   )
 }
 
 export default Home
+
