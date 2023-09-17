6f7eccb23edd9466b8240ecaf18f8b821d19cad7
Dheeraj-2205 <djsays124@gmail.com>
2023-09-17 23:49:01 +0530

    adding css

--- frontend/src/component/Product/ProductDetails.jsx
+++ frontend/src/component/Product/ProductDetails.jsx
@@ -1,85 +1,94 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import "./productDetails.css";
-import { useDispatch, useSelector } from 'react-redux';
-import { getProductDetails } from '../actions/productAction';
-import { useParams } from 'react-router-dom';
-import ReactStars from "react-rating-stars-component"
+import { useDispatch, useSelector } from "react-redux";
+import { getProductDetails } from "../actions/productAction";
+import { useParams } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
 
 const ProductDetails = () => {
     const dispatch = useDispatch();
-    const {id} = useParams();
-    const {product,loading,error} =  useSelector((state)=>state.productDetails);
+    const { id } = useParams();
+    const { product, loading, error } = useSelector(
+        (state) => state.productDetails
+    );
     console.log(product);
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         dispatch(getProductDetails(id));
-    }, [dispatch,id]);
+    }, [dispatch, id]);
 
     const options = {
-        edit : false,
-        color : "rgba(20,20,20,0.1)",
-        activeColor : "tomato",
-        size : window.innerWidth < 600 ? 20 : 25,
+        edit: false,
+        color: "rgba(20,20,20,0.1)",
+        activeColor: "tomato",
+        size: window.innerWidth < 600 ? 20 : 25,
         value: product.rating,
-        isHalf : true
-    }
-  return (
-    <>
-        <div className='productDetails'>
-            <div>
-                <Carousel>
-                    {
-                        product.image && 
-                        product.image.map((item,i)=>{
-                            return (
-                                <img 
-                                    className='CarouselImage'
-                                    key = {i}
-                                    src= {item.url}
-                                    alt={`Slide ${i + 1}`}
-                                />
-                            )
-                        })
-                    }
-                </Carousel>
-            </div>
-        </div>
-
-        <div>
-            <div className="detailsBlock-1">
-                <h2>Product.name</h2>
-                <p>Product # {product._id}</p>
-            </div>
-            <div className='detailsBlock-2'>
-                <ReactStars {...options} />
-                <span>({product.numOfReviews} Reviews)</span>
-            </div>
-            <div className="detailsBlock-3">
-                <h1>{product.price}</h1>
-                <div className="detailsBlock-3-1">
-                    <div className="detailsBlock-3-1-1">
-                        <button>-</button>
-                        <input type="number" value = "1"/>
-                        <button>+</button>
-                    </div>{" "}
-                    <button>Add To Cart</button>
+        isHalf: true,
+    };
+    return (
+        <>
+            <div className="productDetails">
+                <div className="productImages">
+                    <Carousel>
+                        {
+                            product.image &&
+                                product.image.map((item, i) => (
+                                    <img
+                                        className="CarouselImage"
+                                        key={i}
+                                        src={item.url}
+                                        alt={`${i} Slide`}
+                                    />
+                                ))
+                        }
+                    </Carousel>
                 </div>
-                <p>
-                    Status : {" "}
-                    <b className={product.stock < 1 ? "redColor" : "greenColor"}>
-                        {product.stock < 1 ? "OutOfStock" : "InStock"}
-                    </b>
-                </p>
-            </div>
 
-            <div className="detailBlock-4">
-                Description : <p>{product.description}</p>
-            </div>
-            <button className='submitReview'>Submit Review</button>
-        </div>
-    </>
-  )
-}
+                <div className="productDetailsContent">
+                    <div className="detailsBlock-1">
+                        <h2>{product.name}</h2>
+                        <p>Product # {product._id}</p>
+                    </div>
+                    <div className="detailsBlock-2">
+                        <ReactStars {...options} />
+                        <span className="detailsBlock-2-span">
+                            {" "}
+                            ({product.numOfReviews} Reviews)
+                        </span>
+                    </div>
+                    <div className="detailsBlock-3">
+                        <h1>{`₹${product.price}`}</h1>
+                        <div className="detailsBlock-3-1">
+                            <div className="detailsBlock-3-1-1">
+                                <button >-</button>
+                                <input readOnly type="number" value="1" />
+                                <button>+</button>
+                            </div>
+                            <button>
+                                Add to Cart
+                            </button>
+                        </div>
 
-export default ProductDetails
+                        <p>
+                            Status:
+                            <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                                {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                            </b>
+                        </p>
+                    </div>
+
+                    <div className="detailsBlock-4">
+                        Description : <p>{product.description}</p>
+                    </div>
+
+                    <button className="submitReview">
+                        Submit Review
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProductDetails;
