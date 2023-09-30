b45bce8a47fa050e701b78821a1e7b4efc0e3c3a
Dheeraj-2205 <djsays124@gmail.com>
2023-09-30 22:46:35 +0530

    register in redux

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -2,24 +2,32 @@ import {
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
+
 } from "../constants/userConstants.js";
 
 export const userReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case LOGIN_REQUEST:
+    case REGISTER_USER_REQUEST:
+
       return {
         loading: true,
         isAuthenticated: false,
       };
 
+    case REGISTER_USER_SUCCESS :
     case LOGIN_SUCCESS:
+
       return {
         ...state,
         loading: false,
         isAuthenticated: true,
         user: action.payload
       };
-
+    case REGISTER_USER_FAIL: 
     case LOGIN_FAIL:
       return {
         ...state,
